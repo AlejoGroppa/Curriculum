d5ee4dc4f5ec94e1b4834199b38f0680681d6ade
Alejo Groppa <agroppa@psa.local>
2022-01-25 17:00:10 -0300

    cambiada cosas de la pantalla principal

--- src/resume.js
+++ src/resume.js
@@ -6,7 +6,7 @@ const resume = {
             company: 'Policia de Seguridad Aeroportuaria',
             location: 'Remoto - presencial',
             time: '2019 - actualmente',
-            description: 'Miembro del equipo de desarrollo de la **Policia de Seguridad Aeroportuaria** en el puesto de **Desarrollador web jr**, haciendo tareas **full Stack**. Participé en varios sistemas como por ejemplo: **SiCOVID** Sistema utilizado para registrar declaraciones juradas de los pasajeros. **Logistica** Sistema logistico utilizado para gestionar bienes, declarar su ubicacion, el estado y demas detalles. Entre otros sistemas',
+            description: 'Miembro del equipo de desarrollo de la **Policia de Seguridad Aeroportuaria** en el puesto de **Desarrollador web jr**, haciendo tareas **full Stack**. Participé en varios sistemas como por ejemplo: **SiCOVID** Sistema utilizado para registrar declaraciones juradas de los pasajeros. **Logística** Sistema logistico utilizado para gestionar bienes, declarar su ubicación, el estado y demás detalles. Entre otros sistemas',
             technologies: [
                 'Laravel',
                 'Gitlab',
@@ -16,7 +16,7 @@ const resume = {
 
             sistemas: [
                 'SiCOVID',
-                'Logistica Policial',
+                'Logística  Policial',
                 'Portal DGT',
                 'Guia desarrollo',
                 
